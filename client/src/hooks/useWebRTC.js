4ba035577eb151c44f053bb266332be53561a307
Rajin Khan <rajin.khan2001@gmail.com>
2025-08-15 00:00:03 +0600

    TURN Server Maintenance

--- client/src/hooks/useWebRTC.js
+++ client/src/hooks/useWebRTC.js
@@ -4,8 +4,8 @@ import { useState, useEffect, useRef, useCallback } from 'react';
 
 
 export const turnCredentials = {
-  lastResetTimestamp: '2025-08-14 00:00', // <--- UPDATE THIS (e.g., "2024-08-02 15:30")
-  username: "3f405a4f22c257be8ad413dafa99d567f062e86ee9bc2d206e16c528999b65bd", // <--- UPDATE THIS
+  lastResetTimestamp: '2025-08-15 00:00', // <--- UPDATE THIS (e.g., "2024-08-02 15:30")
+  username: "5ddb866b334204eaeeaf628cdbfd991c25812f263efd8db1cbad3ea84146c86c", // <--- UPDATE THIS
   credential: "3C3l6a1dDL7UvJyp21+lObd7QT23PPEtNuqX7ZQboHs=", // <--- UPDATE THIS
 };
 
